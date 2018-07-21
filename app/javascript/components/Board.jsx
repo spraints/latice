8fc0b2e0a9dbb033197b6a8924efc3795b6e2da4
Matt Burke <spraints@gmail.com>
2018-07-21 18:03:37 -0400

    Improve board display

--- app/javascript/components/Board.jsx
+++ app/javascript/components/Board.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 const Cell = (props) => {
-  if (props.sun) return <span className="cell sun">☀️</span>
-  if (props.moon) return <span className="cell moon">🌙</span>
-  return <span className="cell">⬜️</span>
+  if (props.sun) return <span className="board-square sun">☀️</span>
+  if (props.moon) return <span className="board-square moon">🌙</span>
+  return <span className="board-square">⬜️</span>
 }
 
-const Row = (props) => (<div>{props.cells.map(cell => <Cell key={cell.col} {...cell} />)}</div>)
+const Row = (props) => (<div className="board-row">{props.cells.map(cell => <Cell key={cell.col} {...cell} />)}</div>)
 
 export default (props) => {
   const board = props.game.board
   return (
-    <div>
+    <div className="board">
       {board.map(row => <Row key={row.row} {...row} />)}
     </div>
   )
