fff071a4f46058ed8ec6473747cf34282cffbc17
Matt Burke <spraints@gmail.com>
2018-07-29 15:22:58 -0400

    MyArea is a component now

--- app/javascript/components/MyArea.jsx
+++ app/javascript/components/MyArea.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 import Tile from './Tile'
 
-export default (props) => {
-  return (
-    <div>
-      <div className="rack">
-        <h4>My tiles</h4>
-        {props.game.racked_tiles.map(tile => <Tile key={tile.id} {...tile} />)}
+export default class MyArea extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="rack">
+          <h4>My tiles</h4>
+          {this.props.game.racked_tiles.map(tile => <Tile key={tile.id} {...tile} />)}
+        </div>
+        <div className="pool">
+          {this.props.game.current_player.tile_counts.total - this.props.game.current_player.tile_counts.in_rack} more tiles
+        </div>
       </div>
-      <div className="pool">
-        {props.game.current_player.tile_counts.total - props.game.current_player.tile_counts.in_rack} more tiles
-      </div>
-    </div>
-  )
+    )
+  }
 }
